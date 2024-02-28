4ca1b2602715a8e3e21699a476d1d687a74fb945
atikhomsiri <atikhom.s@gmail.com>
2024-02-29 01:48:15 +0700

    Change PG to Singapore #2

--- controllers/db.js
+++ controllers/db.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
 
-// .singapore-postgres.render.com
-
 const pool = new Pool({
     user: 'thesaban_pg_user',
     password: 'wtMit6gxJaOLHwajNqfwiVtEiPGWAhMM',
-    host: 'dpg-cnfnrvqcn0vc73ark6r0-a',
+    host: 'dpg-cnfnrvqcn0vc73ark6r0-a.singapore-postgres.render.com',
     port: '5432',
     database: 'thesaban_pg',
     ssl: true
