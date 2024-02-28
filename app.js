c34abaa11b19c516952ed2d9434f5ce5779cd77d
atikhomsiri <atikhom.s@gmail.com>
2024-02-29 00:56:14 +0700

    EDIT HOST SSL PG

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("view engine","ejs");
 const client = new Client({
     user: 'sql_thesaban_user',
     password: 's4q0wCqmsxZZbhLvf9EgGrUC2FvU05pl',
-    host: 'dpg-cnakvida73kc73eng0v0-a',
+    host: 'dpg-cnakvida73kc73eng0v0-a.oregon-postgres.render.com',
     port: '5432',
     database: 'sql_thesaban',
     ssl: true
