358be6fcd362b0b864a2e3580f1348497fbe1384
atikhomsiri <atikhom.s@gmail.com>
2024-02-29 00:39:24 +0700

    ADD DOTENV

--- app.js
+++ app.js
@@ -4,29 +4,8 @@ const { Client } = require('pg');
 
 app.set("view engine","ejs");
 
-const client = new Client({
-    user: 'sql_thesaban_user',
-    password: 'passwords4q0wCqmsxZZbhLvf9EgGrUC2FvU05pl',
-    host: 'dpg-cnakvida73kc73eng0v0-a.oregon-postgres.render.com',
-    port: '5432',
-    database: 'sql_thesaban',
-  });
-
-  client.query('SELECT * FROM hardware', (err, result) => {
-  if (err) {
-    console.error('Error executing query', err);
-  } else {
-    console.log('Query result:', result.rows);
-  }
-  });
-
-  client.connect()
-  .then(() => {
-    console.log('Connected to PostgreSQL database');
-  })
-  .catch((err) => {
-    console.error('Error connecting to PostgreSQL database', err);
-  });
+const dotenv = require('dotenv');
+dotenv.config();
 
 const iotRoute = require("./routes/iotRoute");
 app.use("/",iotRoute);
