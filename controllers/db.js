8a47651c54e47e1e52a8f72f2e15a82ba99b1a70
atikhomsiri <atikhom.s@gmail.com>
2024-02-29 01:46:20 +0700

    Change PG to Singapore

--- controllers/db.js
+++ controllers/db.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
+// .singapore-postgres.render.com
+
 const pool = new Pool({
-    user: 'sql_thesaban_user',
-    password: 's4q0wCqmsxZZbhLvf9EgGrUC2FvU05pl',
-    host: 'dpg-cnakvida73kc73eng0v0-a.oregon-postgres.render.com',
+    user: 'thesaban_pg_user',
+    password: 'wtMit6gxJaOLHwajNqfwiVtEiPGWAhMM',
+    host: 'dpg-cnfnrvqcn0vc73ark6r0-a',
     port: '5432',
-    database: 'sql_thesaban',
+    database: 'thesaban_pg',
     ssl: true
 });
 
