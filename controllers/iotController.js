84eb716a0c5cc64fd7f5138d8044f64e01a0ba9a
atikhomsiri <atikhom.s@gmail.com>
2024-02-29 12:07:27 +0700

    INFLUX TETS  ADD

--- controllers/iotController.js
+++ controllers/iotController.js
@@ -1,5 +1,38 @@
 const controller = {};
 const conn = require('./db');
+const {InfluxDB, Point} = require('@influxdata/influxdb-client');
+
+const token = "pUSqccneRAiNvKTtiTdnhnlbYynyUcPLtffybhAaZ3Wp6FvlZz15IPu201jKaXfPctbW4iJGeaFZm4mrxgZuGw=="
+const url = 'https://eu-central-1-1.aws.cloud2.influxdata.com'
+const client = new InfluxDB({url, token})
+let org = `e266c442dcee6ac5`
+let bucket = `THESABAN`
+
+controller.iot = (req,res) => { 
+    let queryClient = client.getQueryApi(org)
+    let fluxQuery = `from(bucket: "THESABAN")
+     |> range(start: -5m)
+     |> filter(fn: (r) => r._measurement == "dht")
+     |> filter(fn: (r) => r._field == "temp")
+     |> last()`
+     
+    queryClient.queryRows(fluxQuery, {
+      next: (row, tableMeta) => {
+        const tableObject = tableMeta.toObject(row)
+        console.log(tableObject._value);
+        
+      },
+      error: (error) => {
+        console.error('\nError', error)
+      },
+      complete: () => {
+       
+        res.end("IOT Hello!");
+        
+      },
+    })
+    
+  };
 
 controller.project = (req,res) => { 
    
@@ -12,9 +45,5 @@ controller.project = (req,res) => {
 
 };
 
-controller.iot = (req,res) => { 
-   
-  res.end("IOT Hello!");
-};
 
 module.exports = controller;
