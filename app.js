a2b34b16085212885b16b8d890cc3756daad3d02
atikhomsiri <atikhom.s@gmail.com>
2024-03-01 08:18:45 +0700

    edit app.js

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 
 app.set("view engine","ejs");
+
 const session = require("express-session");
+app.use(session({secret:"Passw0rd"}));
+
+const body = require("body-parser");
+app.use(body());
+
+
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 const iotRoute = require("./routes/iotRoute");
 app.use("/",iotRoute);
