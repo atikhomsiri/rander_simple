c637f3fd5b200bd75e85edd09975c65572374a3f
atikhomsiri <atikhom.s@gmail.com>
2024-03-01 08:14:55 +0700

    add express session

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine","ejs");
- 
+const session = require("express-session");
 
 const iotRoute = require("./routes/iotRoute");
 app.use("/",iotRoute);
